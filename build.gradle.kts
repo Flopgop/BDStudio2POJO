b0be9cc7e8ea4419ef182f136857d69317466ebf
Not Flamgop <76978462+Flopgop@users.noreply.github.com>
2024-10-16 03:34:45 -0700

    Fix jitpack stuffs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
+    implementation("com.github.jitpack:gradle-simple:1.0")
     implementation("com.google.code.gson:gson:2.11.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
